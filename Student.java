ed5d31f2ac040083b63148664ae7592ca2248fdb
Jeremias Esperanza <142370600+JerryEsperanza@users.noreply.github.com>
2024-02-10 04:20:38 +0000

    my first commit

--- Student.java
+++ Student.java
@@ -2,5 +2,30 @@ public class Student{
     private String studentNo;
     private String lastName;
     private String firstName;
+
+    
+    public Student(String studentNo, String lastName, String firstName) {
+        this.studentNo = studentNo;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+    public String getStudentNo() {
+        return studentNo;
+    }
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
     
 }
